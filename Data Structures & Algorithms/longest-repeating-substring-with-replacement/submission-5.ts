2c62e48894d099fed02379d6b4cbc3bdd3252a76
Jeremiah Laza <79945370+remsysc@users.noreply.github.com>
2026-08-11 19:48:17 +0800

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.ts
@@ -0,0 +1,26 @@
+class Solution {
+  /**
+   * @param {string} s
+   * @param {number} k
+   * @return {number}
+   */
+  characterReplacement(s: string, k: number): number {
+    let count: { [key: string]: number } = {};
+    let res = 0;
+    let left = 0;
+    let maxF = 0;
+
+    for (let right = 0; right < s.length; right++) {
+      count[s[right]] = (count[s[right]] || 0) + 1;
+      maxF = Math.max(maxF, count[s[right]]);
+
+      if (right - left + 1 - maxF > k) {
+        count[s[left]]--;
+        left++;
+      }
+      res = Math.max(res, right - left + 1);
+    }
+
+    return res;
+  }
+}
